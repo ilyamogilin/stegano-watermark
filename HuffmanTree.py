5304356886cb1eeb9104edf6c2c09493f6c16514
Ilya Mogilin <ilya.mogilin@phystech.edu>
2017-12-09 01:59:58 +0300

    Started working on JPEG support.

---
+++ HuffmanTree.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3
+
+class HuffmanNode(object):
+    def __init__(self, left = None, right = None, root = None):
+        self.left = left
+        self.right = right
+        self.root = root
+
+def create_tree(node, quan, codes, level, subnodes_count):
+    if (len(codes) == 0):
+        return
+    else:
+        if (node.left == None):
+            if (subnodes_count < quan[level + 1]):
+                node.left = codes[0]
+                print(codes[0])
+                create_tree(node, quan, codes[1::], level, subnodes_count + 1)
+            else:
+                node.left = HuffmanNode(root = node)
+                create_tree(node.left, quan, codes, level + 1, 0)
+        elif (node.right == None):
+            if (subnodes_count < quan[level + 1]):
+                node.right = codes[0]
+                print(codes[0])
+                create_tree(node, quan, codes[1::], level, subnodes_count + 1)
+            else:
+                node.right = HuffmanNode(root = node)
+                create_tree(node.right, quan, codes, level + 1, 0)
+        else:
+            print('root')
+            create_tree(node.root, quan, codes, level - 1, quan[level])
--- misc.py
+++ misc.py
@@ -21,3 +21,17 @@ def format_data(string):
 def reformat_data(string):
     n = int(string, 2)
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+
+def get_diag_index(sum):
+    if (sum <= 7):
+        return 0
+    elif (sum == 14):
+        return 7
+    else:
+        return sum % 7
+
+def get_diag_sum(sum):
+    if (sum <= 7):
+        return sum
+    else:
+        return 7
